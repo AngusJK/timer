ec2f899cd28912c4ebe96bffb4c3083e2995d64d
Angus Kellett <anguskellett@hotmail.com>
2020-11-11 02:29:29 +0000

    can call function using natural numbers only

--- timer1.js
+++ timer1.js
@@ -1,10 +1,15 @@
-// produces a ding at 3, 5, 9, 10, and 15 second intervals
+const args = process.argv.splice(2);
+const milliseconds = args.map(function(num) { return num * 1000 });
+console.log(milliseconds);
 
 const timer = function(x) {
-  setTimeout = (() => 
-  process.stdout.write('\x07'), x);
+  for (let i = 0; i < x.length; i++) {
+    console.log(x[i]);
+  setTimeout(() => { process.stdout.write('\x07'); }, x[i]);
+  };
 };
 
-timer();
+timer(milliseconds);
+
 
 // node timer1.js 10 3 5 15 9
