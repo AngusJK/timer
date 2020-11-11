acca90e25334d20c2d05594b215eeab0c5638692
Angus Kellett <anguskellett@hotmail.com>
2020-11-11 18:43:27 +0000

    refactors and cleans up formatting using eslint

--- timer1.js
+++ timer1.js
@@ -1,20 +1,13 @@
 const args = process.argv.splice(2);
-const milliseconds = args.filter(number => number > 0).map(function(num) { return num * 1000 });
-console.log(milliseconds);
+const milliseconds = args.filter(num => num > 0).map(num => num * 1000);
 
-const timer = function(x) {
-  for (let i = 0; i < x.length; i++) {
-    console.log(x[i]);
-  setTimeout(() => { process.stdout.write('\x07'); }, x[i]);
-  };
+const timer = function(timesArray) {
+  for (let i in timesArray) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, timesArray[i]);
+  }
 };
 
 timer(milliseconds);
 
-// tests
-
-// node timer1.js 10 3 4 15 -8 9 
-// node timer1.js 0
-// node timer1.js 3 "4" 5
-// node timer1.js 3 "hello" 5
-// node timer1.js
